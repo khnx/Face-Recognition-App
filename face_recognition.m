4c9888e43e2825359604394863714c7d240061c7
Radosław Smoter <radoslaw.sm.tr@gmail.com>
2022-01-01 22:44:35 +0100

    update

--- face_recognition.m
+++ face_recognition.m
@@ -1,5 +1,19 @@
 clc; clear;
 
-data = load_data();
+img_data = load_data();
 
-[coeff, score, latent, tsquared, explained, mu] = pca(data);
+[coeff, score, latent, tsquared, explained, mu] = pca(double(img_data));
+
+% number of significant PCAs
+n_components = 1;
+while explained(n_components,1) > 1
+    n_components = n_components +1;
+end
+n_components = n_components -1;
+
+% all images - mean image
+img_reduced = single(img_data) - single(mu);
+eigenfaces = single(img_reduced) * single(coeff);
+
+% calculate weights
+img_size = size(img_data(:, 1), 1);
--- load_data.m
+++ load_data.m
@@ -8,24 +8,20 @@ function data = load_data()
     % container for all photos
     img_size_x = 112;
     img_size_y = 92;
-    all_img = zeros(img_size_x*img_size_y, n_dir*10);
+    all_img = zeros(img_size_x*img_size_y, n_dir*10, 'uint8');
     
     for ii = 1:n_dir    % up to number of models
         cd(strcat('s',num2str(ii)));    % change to ii-th directory
         for jj = 1:10   % 10 photos for each model
-            img = imread(strcat(num2str(jj), ".pgm"));
-            % resize to correct x/y sizes
-            img_2d = imresize(img, [img_size_x, img_size_y]);
+            img = uint8(imread(strcat(num2str(jj), ".pgm")));
             % reshape to (x * y) x 1 size
-            img_1d = reshape(img_2d, img_size_x * img_size_y, 1);
-            all_img(:, (ii - 1) * 10 + jj) = img_1d;
+            img_1d = reshape(img, img_size_x * img_size_y, 1);
+            all_img(:, (ii-1)*10+jj) = img_1d;
         end
         cd ..;
     end
     disp("Database Loaded Successfully!");
     % return to the beginning directory
     cd ..;
-    % format for pca()
-    data = reshape(all_img, n_dir*10, img_size_x*img_size_y);
     data = all_img;
 end
